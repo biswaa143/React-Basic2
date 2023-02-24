0ebd2532d412c1223ee7fb109d50850fa1389732
Biswa <barikbiswa143@gmail.com>
2023-02-24 13:37:59 +0530

    button added

--- my-app/src/components/Expenses/ExpenseItem.js
+++ my-app/src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,9 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log('Deleted');
+  };
 
   return (
     <Card className="expense-item">
@@ -12,6 +15,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">₹ {props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Delete Expense</button>
     </Card>
   );
 }
