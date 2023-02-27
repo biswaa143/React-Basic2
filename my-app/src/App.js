076dced38e25e7f6b1b0aeb1ff4b9205ab9c78bc
Biswa <barikbiswa143@gmail.com>
2023-02-27 15:06:36 +0530

    Rendering expenses

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import NewExpense from "./components/Expenses/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-const App = () => {
-  const expenses = [
-    {
-      title: "Car Insurance",
-      amount: 15000,
-      date: new Date(2023, 2, 23),
-    },
-    {
-      title: "Electric Bill",
-      amount: 5000,
-      date: new Date(2022, 3, 30),
-    },
-    {
-      title: "Fooding",
-      amount: 10000,
-      date: new Date(2021, 4, 10),
-    },
-    {
-      title: "Gaming Setup",
-      amount: 250000,
-      date: new Date(2020, 5, 13),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: "Car Insurance",
+    amount: 15000,
+    date: new Date(2023, 2, 23),
+  },
+  {
+    id: 'e2',
+    title: "Electric Bill",
+    amount: 5000,
+    date: new Date(2022, 3, 30),
+  },
+  {
+    id: 'e3',
+    title: "Fooding",
+    amount: 10000,
+    date: new Date(2021, 4, 10),
+  },
+  {
+    id: 'e4',
+    title: "Gaming Setup",
+    amount: 250000,
+    date: new Date(2020, 5, 13),
+  },
+];
 
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   //Using JSX
--- my-app/src/components/Expenses/ExpenseItem.js
+++ my-app/src/components/Expenses/ExpenseItem.js
@@ -22,7 +22,7 @@ const ExpenseItem = (props) => {
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <div className="expense-item__price">₹ {props.amount}</div>
       </div>
       {/* <button onClick={clickHandler}>Change Title</button>
       <button onClick={clickHandler2}>Change Amount</button> */}
