64810cb0b48df6776a838caf76337f1274d23949
Biswa <barikbiswa143@gmail.com>
2023-03-02 13:14:17 +0530

    DIY-Challenge on conditional rendering

--- my-app/src/components/Expenses/NewExpense/ExpenseForm.js
+++ my-app/src/components/Expenses/NewExpense/ExpenseForm.js
@@ -68,6 +68,7 @@ const ExpenseForm = (props) => {
     setEnteredAmount("");
     setEnteredDate("");
   };
+  
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
@@ -93,14 +94,15 @@ const ExpenseForm = (props) => {
           <label>Date</label>
           <input
             type="date"
-            min="2023-01-01"
-            max="2026-12-31"
+            min="2019-01-01"
+            max="2024-12-31"
             value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
